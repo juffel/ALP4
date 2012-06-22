960b6d376a55311aba95f9d93aaa0119c7bf29dc
juffel <julian.dobmann@googlemail.com>
2012-06-22 13:58:02 +0200

    added Philosopher methods

--- src/uebung09/diningPhilosophers.java
+++ src/uebung09/diningPhilosophers.java
@@ -1,24 +1,52 @@
 package uebung09;
 
+import java.util.Random;
+
 public class diningPhilosophers {
 	
 	public static class Philomonitor {
 
+		/** 
+		 * zwei Gabeln nehmen
+		 * @param id
+		 */
 		public void getForks(int id) {
 			
 		}
 		
+		/** 
+		 * zwei Gabeln wieder freigeben
+		 * @param id
+		 */
 		public void releaseForks(int id) {
 			
 		}
 
 	}
 	
+	/**
+	 * Philosophen essen und pausieren(philosophieren) in unregelmäßigen
+	 * Abständen
+	 * @author julian
+	 *
+	 */
 	public class Philosopher implements Runnable {
-
-		@Override
+		
+		Random rnd = new Random();
+		
 		public void run() {
-			// TODO Auto-generated method stub
+			
+			while(true) {
+				think();
+				eat();
+			}
+		}
+		
+		public void think() {
+			
+		}
+		
+		public void eat() {
 			
 		}
 		
